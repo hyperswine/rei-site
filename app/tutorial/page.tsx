be050162f2b403607bc50fd7fb49f0ffe05a54ee
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-11-22 04:16:13 +1100

    Add stuff

---
+++ app/tutorial/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { Button, ChakraProvider } from "@chakra-ui/react"
+
+export default function Page() {
+  return (
+    <>
+      <Button />
+      <>Rei Tutorial</>
+    </>
+  )
+}
